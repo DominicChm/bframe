8ce469d65e6b8a306a705e3fd36e8aeecdbf58b1
Dominic C <45153623+DominicChm@users.noreply.github.com>
2021-05-04 14:53:09 -0700

    Add runtime ID provider tests

--- backend/src/modules/particle-manager/runtime-id-provider/RuntimeIdProvider.ts
+++ backend/src/modules/particle-manager/runtime-id-provider/RuntimeIdProvider.ts
@@ -7,15 +7,23 @@ export class RuntimeIdProvider {
     /**
      * Allocates an ID linked to an input UID.
      */
-    allocRid(uid: string): number {
-        if (this.uid_rid_map[uid]) { // If UID already connected, return allocated id.
+    allocRid(uid: string): number | undefined {
+        // If UID already connected, return allocated id.
+        if (!uid) return undefined;
+
+        if (this.uid_rid_map[uid] !== undefined) {
             return this.uid_rid_map[uid];
         }
 
-        const i = this.rid_uid_map.indexOf(undefined); //Get index of undefined to find unused rID
+        if (uid.length < 3) {
+            throw new Error("UIDs must be at least 3 characters!")
+        }
+
+        //Get index of undefined to find first unallocated rID
+        const i = this.rid_uid_map.indexOf(undefined);
 
         if (i < 0) {
-            throw new Error("No IDs left to assign!");
+            return undefined;
         }
 
         this.uid_rid_map[uid] = i;
@@ -24,8 +32,29 @@ export class RuntimeIdProvider {
         return i;
     }
 
-    deallocRid(uid: string) {
-        throw new Error("unimplemented")
+    deallocRid(rid: number)
+    deallocRid(uid: string)
+    deallocRid(id: string | number): boolean {
+        if (typeof id === "number") {
+            if (!this.containsRid(id)) return;
+
+            const uid = this.rid_uid_map[id]
+            delete this.rid_uid_map[id];
+            delete this.uid_rid_map[uid];
+
+            return true;
+        } else if (typeof id === "string") {
+            if (!this.containsUid(id)) return;
+
+            const rid = this.uid_rid_map[id]
+            delete this.rid_uid_map[rid];
+            delete this.uid_rid_map[id];
+
+            return true;
+        } else {
+            return false;
+            //throw new Error(`Couldn't deallocate from ${id}`);
+        }
     }
 
     resolveUidFromRid(rid: number): string {
@@ -34,5 +63,13 @@ export class RuntimeIdProvider {
 
     resolveRidFromUid(uid: string): number {
         return this.uid_rid_map[uid];
+    }
+
+    containsUid(uid: string) {
+        return this.uid_rid_map[uid] !== undefined;
+    }
+
+    containsRid(rid: number) {
+        return this.rid_uid_map[rid] !== undefined;
     }
 }
