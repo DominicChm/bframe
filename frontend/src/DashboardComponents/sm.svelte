e7b2290ce295b33f166bbf266f5ec1d3b0908d16
Nathan Lee <nathan.l.lee123@gmail.com>
2021-05-05 13:21:07 -0700

    Started prototyping a dashboard and establishing a visual style

---
+++ frontend/src/DashboardComponents/sm.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+   export let title:string;
+   export let viz:any;
+</script>
+
+<div id="dashComp" class="sm">
+   <div class="Title">{title}</div>
+   <div class="vizCont">
+      <svelte:component this={viz} />
+   </div>
+   
+</div>
+
+<style>
+   .sm {
+      width: 360px;
+      height: 360px;
+      
+   }
+
+   .vizCont {
+      text-align: center;
+
+   }
+   
+</style>
---
+++ frontend/src/DataViz/number.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+   let curNum:number = 30;
+
+   setInterval(() => {
+      curNum = Math.floor(Math.random() * 10) 
+   }, 500)
+</script>
+
+<div id="numberContainer">
+   <div id="numberViz">{curNum}</div>
+   <h4>Miles an hour</h4>
+</div>
+
+
+<style>
+   #numberContainer {
+      text-align: center;
+      padding-top: 50px;
+
+   }
+
+   #numberViz {
+      font-size:150px;
+      font-weight: bold;
+      text-align: center;
+      
+   }
+
+   h4 {
+      margin-left:auto;
+      font-size: 30px;
+   }
+</style>
