2e9803f8c72bb116a8e4123665fef7bce9cdeae2
Dominic C <45153623+DominicChm@users.noreply.github.com>
2021-05-06 08:22:25 -0700

    Various changes to particlemanager

--- backend/src/modules/particle-manager/ParticleManager.ts
+++ backend/src/modules/particle-manager/ParticleManager.ts
@@ -1,37 +1,85 @@
-import WebSocket from "ws";
-import {compose_HANDSHAKE_RESPONSE, IParticleHandshake} from "../protocol";
+import WebSocket, {AddressInfo} from "ws";
+import {compose_ERROR, compose_HANDSHAKE_RESPONSE, IParticleHandshake} from "../protocol";
 import {handle_handshake} from "bc/protocol";
 import {RuntimeIdProvider} from "./runtime-id-provider/RuntimeIdProvider";
 import {ParticleDefinitionRegistry} from "./particle-def-registry/ParticleDefinitionRegistry";
 import {logger} from "bc/logging";
+import {IParticleManagerOptions} from "./interfaces/IParticleManagerOptions";
+import {ParticleConnectionManager} from "./particle-connection/ParticleConnectionManager";
+import EventEmitter from "events";
+import {IParticleStatePatchEvent} from "./interfaces/PMEvents/IParticleStatePatchEvent";
+import {ISystemDefinition} from "./interfaces/ISystemDefinition";
 
 const log = logger("particle manager")
 
-export class ParticleManager {
-    private wss: WebSocket.Server;
-    private rid_provider: RuntimeIdProvider = new RuntimeIdProvider();
-    private pdef_registry: ParticleDefinitionRegistry = new ParticleDefinitionRegistry();
 
-    constructor() {
+export class ParticleManager extends EventEmitter {
+    private wss: WebSocket.Server;
+    private ridProvider: RuntimeIdProvider = new RuntimeIdProvider();
+    private particleConnections: { [key: string]: ParticleConnectionManager<any, any> } = {};
+    private particleDefRegistry: ParticleDefinitionRegistry = new ParticleDefinitionRegistry();
+
+    //Expose registry methods
+    public registerDefinition = this.particleDefRegistry.registerDefinition.bind(this.particleDefRegistry);
+    public getDefByTypename = this.particleDefRegistry.getDefByTypename.bind(this.particleDefRegistry);
+
+    public readonly port;
+
+    constructor(options: IParticleManagerOptions) {
+        super();
         log("Starting websocket server");
-        this.wss = new WebSocket.Server({port: 369});
+        this.wss = new WebSocket.Server({port: options.port});
+        this.port = (this.wss.address() as AddressInfo).port;
+
+        //Setup initial listeners
         this.wss.on("connection", (socket) => {
                 log("Incoming connection")
-                handle_handshake(socket, this.handleHandshake)
+                handle_handshake(socket, this.handleHandshake.bind(this))
             }
         );
     }
 
+    loadSystem(sys: ISystemDefinition) {
+        console.log("Loading new system");
+        this.particleConnections = {};
+        for (const par of sys.particles) {
+            const def = this.particleDefRegistry.getDefByTypename(par.typeName);
+            if (!def) console.error(`Particle with UID >${par.uid}< was not loaded because typeName >${par.typeName}< wasn't found.`);
+
+            this.particleConnections[par.uid] = new ParticleConnectionManager(def);
+        }
+    }
+
     handleHandshake(particle_info: IParticleHandshake, ws: WebSocket): Buffer {
-        const def = this.pdef_registry.getDefByTypename(particle_info.type);
+        const def = this.particleDefRegistry.getDefByTypename(particle_info.typeName);
         if (!def) {
-            //TODO: Fail gracefully.
-            throw new Error(`No definition exists for typename ${particle_info.type}`);
+            throw new Error(`Handshake error: No definition exists for ${particle_info.typeName}`);
         }
 
-        const rid = this.rid_provider.allocRid(particle_info.uid);
+        if (this.particleConnections[particle_info.uid]) {
+            this.particleConnections[particle_info.uid].terminate();
+        }
 
+        const pcm = new ParticleConnectionManager(def);
+        this.particleConnections[particle_info.uid] = pcm;
+
+        const rid = this.ridProvider.allocRid(particle_info.uid);
         return compose_HANDSHAKE_RESPONSE(rid);
     }
 
+    /**
+     * Stops the ParticleManager so it doesn't hang the node process.
+     */
+    stop() {
+        this.wss.close()
+    }
+
+    particle(uid: string) {
+
+    }
+
+    on(event: "patch", listener: (patch: Object) => void);
+    on(event, listener) {
+
+    }
 }
