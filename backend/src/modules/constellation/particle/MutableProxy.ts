dbbe5ff78e1f21ec61e28f9e4c3654d5bf730164
Dominic C <45153623+DominicChm@users.noreply.github.com>
2021-06-20 10:16:40 -0700

    Doc improvements + patch functionality improvement

--- backend/src/modules/constellation/particle/MutableProxy.ts
+++ backend/src/modules/constellation/particle/MutableProxy.ts
@@ -1,9 +1,23 @@
 import _, {cloneDeep} from "lodash"
 
-
+/**
+ * A proxy handler that has additional functionality to track state before and after object mutation through a proxy.
+ * It provides the current object state, the original object state, and what has changed in between the two (patch)
+ */
 export class MutableProxy<T extends Object> implements ProxyHandler<T> {
+    /**
+     * The original state of the object, before changes were recorded
+     */
     public previous: T;
+
+    /**
+     * An object containing the fields and values that have changed between the original and current state
+     */
     public patch: Partial<T> = {};
+
+    /**
+     * The current state of the object
+     */
     public current: T;
 
     private _keyDenyList: Set<string>
@@ -26,15 +40,19 @@ export class MutableProxy<T extends Object> implements ProxyHandler<T> {
         this._init(target);
 
         //Do not set blocked fields
-        if(this._keyDenyList.has(key))
+        if (this._keyDenyList.has(key))
             throw new Error(`key deny list has >${key}<`);
 
         //Only set existing fields.
-        if(target[key] === undefined)
+        if (target[key] === undefined)
             throw new Error(`>${key}< is undefined on target object`);
 
+        // Only record as patch if the set value is different to the current.
+        if (target[key] !== value)
+            this.patch[key] = value;
+
         target[key] = value;
-        this.patch[key] = value;
+
         return true;
     }
 
